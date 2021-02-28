bd81778c1d5d936c2466a01833daa9ca8eba4593
Thiago Alves da Silva <39628806+tsilva19@users.noreply.github.com>
2021-02-28 20:32:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
-//'mongodb://host.docker.internal:27017/consulta'
-////fa163e822325l01.ctsp.des.cloud.ihf:31000/
+
+
 mongoose.connect('mongodb://localhost:27017/consulta', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
